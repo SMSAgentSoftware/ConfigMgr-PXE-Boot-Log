141eb819e3fc0dac8bb38378f1181f134711f940
Trevor Jones <SMSAgentSoftware@users.noreply.github.com>
2017-07-17 16:10:27 +0100

    v1.1 changes

--- ConfigMgrPXEBootLog.ps1
+++ ConfigMgrPXEBootLog.ps1
@@ -30,7 +30,6 @@ If ($PSVersionTable.PSVersion.Major -lt 5)
   Break
 }
 
-
 # File hash checks
 $XAMLFiles = @(
     "About.xaml"
@@ -49,14 +48,14 @@ $PSFiles = @(
 
 $Hashes = @{
     "ClassLibrary.ps1" = '1F9C550027197CD5C3DFF2D2C2244BB0B0A31F40B8133164A41AC897754493A9'
-    "EventLibrary.ps1" = 'A3FE17629022C0C0F4EDEF728EDE3DC534EA32251AA903DA34DB71C60CD58F47'
-    "FunctionLibrary.ps1" = '20AEAE75EC37BDEE8B58583B5908DDADDF463A3F7A85D5EDEEE01F5ACFE63E7C'
-    "About.xaml" = 'AC70526C153B8C4E2AE9A8B868F2E1CF5BD1DAE35B18FC2D735E6BFD5F5990D7'
+    "EventLibrary.ps1" = '23CF1E3CA32A23948DA296D7ED0EC00BAB280A4791933C9C461F96345233B963'
+    "FunctionLibrary.ps1" = 'E4A4A3D7E8F929FC2936B8BBDDC5F0FF32EDF0437DAA372130F9CEFD00DA5E9D'
+    "About.xaml" = '7C3065161A3A40244B399C1FB884F2110BF29580584C1E85AD54B762818AFCE9'
     "App.xaml" = '3188DF98290A04FCDF0377BA41361B0884AF786EA0E90D8A48283192DABDF681'
     "Devices.xaml" = '2A01661C76FF449B1A547A7A4F0CEB95FFE60862042107BAAE468CA9B1CD258E'
     "Help.xaml" = 'E52DC0F561D41A74522EBDC7660A77A89606E324BCE74769F27492EAD7797812'
-    "HelpFlowDocument.xaml" = '980192BE7652D2A8154F1985E8B7C6B147AC36B992ECB6704B0B158A394CAA88'
-    "Settings.xaml" = '30D402D1F1A25B45F74CBF8EFF0D3B8CF41E60E188A4C4BFA54A933A52732261'
+    "HelpFlowDocument.xaml" = 'D220A6BBAE5220168E3BB2EBB4781A76EC5F5D670C3CE4C3A36D928EF4A11F78'
+    "Settings.xaml" = 'AFE6F729AE3E237C207759E117861038CFDD8EE4733C54F05506E9EC0DFD37F8'
 }
 
 $XAMLFiles | foreach {
@@ -123,7 +122,7 @@ $UI.SessionData.Add("HKCU:\SOFTWARE\SMSAgent\ConfigMgr PXE Boot Log")
 $UI.SessionData.Add("False")
 $UI.SessionData.Add($null) # not used
 $UI.SessionData.Add($null)
-$UI.SessionData.Add([double]1.0)
+$UI.SessionData.Add([double]1.1)
 $UI.SessionData.Add($Source)
 $UI.SessionData.Add(0)
 $UI.SessionData.Add($null)
@@ -188,7 +187,8 @@ Else
     $windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
     $asyncwindow = Add-Type -MemberDefinition $windowcode -Name Win32ShowWindowAsync -Namespace Win32Functions -PassThru
     $null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0)
- 
+
     $app = New-Object -TypeName Windows.Application
     $app.Run($UI.Window)
+
 }
